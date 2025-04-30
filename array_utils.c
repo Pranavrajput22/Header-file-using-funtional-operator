f270db06395c5a3794d8d05ea9b21df31a094e10
Pranavrajput22 <pranav.dev_cs24@gla.ac.in>
2025-04-30 19:55:24 +0530

    Create array_utils.c

---
+++ array_utils.c
@@ -0,0 +1,27 @@
+#include "array_utils.h"
+
+int find_max(int arr[], int size) {
+    int max = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int array_sum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+void reverse_array(int arr[], int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+}
